887a9b722a886be9724fe7cbd2c3ef846a6ce613
Kelvin Mwatusi <kelvin.mwatusi@Tz.aku.edu>
2024-03-02 13:45:43 +0300

    Your commit message here

---
+++ greetings.py
@@ -0,0 +1,5 @@
+print("Hello World")
+#this is a comment
+print("Hi")
+number = '10'
+print(number)
---
+++ user_input.py
@@ -0,0 +1,10 @@
+name = input('What is your name:')
+print('You entered:', name)
+
+age = input ('what is your age:')
+print('You entered:', age)
+
+
+location = input ('what is your location:')
+print('You entered:', location)
+
