be767a10350ae9fcd53c0d98b9581f55fc6b803d
kelvin.mwatusi <kelvinisack99@gmail.com>
2024-03-02 16:48:12 +0300

    updated file

--- user_input.py
+++ user_input.py
@@ -1,5 +1,5 @@
-name = input('What is your name:')
-print('You entered:', name)
+NAME = input('What is your name:')
+print('You entered:', NAME)
 
 age = input ('what is your age:')
 print('You entered:', age)
