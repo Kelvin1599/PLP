4bf73f5f1d353449bb9b49b19ba75feaddc50c04
kelvin.mwatusi <kelvinisack99@gmail.com>
2024-03-03 14:44:34 +0300

    Your commit message here

--- user_input.py
+++ user_input.py
@@ -1,17 +1,16 @@
 name = input('What is your name:')
 print('You entered:', name)
 
-age = input ('what is your age:')
-print('You entered:', age)
+AGE = input ('what is your age:')
+print('You entered:', AGE)
 
 
 location = input ('what is your location:')
 print('You entered:', location)
 
 def generate_personalized_message(name):
-    message = f"Hello {name} from {location}, you're {age} years old thank you for reaching out to us.  How can I help you?"
+    message = f"Hello {name} from {location}, you're {AGE} years old thank you for reaching out to us.  How can I help you?"
     return message
 
 personalized_message = generate_personalized_message(name)
 print(personalized_message)
-
